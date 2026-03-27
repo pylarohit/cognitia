1486b56b8e862774927c8066de246552ed68f875
pylarohit <pylarohit123@gmail.com>
2026-03-27 19:54:08 +0530

    structure fixed

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "i.pravatar.cc",
+      "pedvkwlatzssmjmgfjyn.supabase.co",
+      "baezjntkxevjzmyuyxqj.supabase.co",
+    ],
+  },
+};
+
+export default nextConfig;
